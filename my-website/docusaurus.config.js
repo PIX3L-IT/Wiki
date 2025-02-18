f823fa4309686f5f6b082aa4dd3051ae56df0820
alexmushi <A01705550@tec.mx>
2025-02-17 22:23:03 -0600

    Título cambiado

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'PIXEL-IT',
+  tagline: 'Soluciones creadas pixel a pixel',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
