39bb7de0635ae7ba72283e3564217a5e7aa5d4e5
alexmushi <A01705550@tec.mx>
2025-02-17 22:08:23 -0600

    Config modificado para hacer deploy en la página de la org

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -22,8 +22,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'PIX3L-IT', // Usually your GitHub org/user name.
+  projectName: 'Documentacion', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
