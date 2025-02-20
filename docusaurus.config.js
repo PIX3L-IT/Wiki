c46b2ed37c7535d265ca6a0f34ef76615b0b20c4
alexmushi <A01705550@tec.mx>
2025-02-20 14:47:12 -0600

    nombre cambiado a Wiki

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Documentacion/',
+  baseUrl: '/Wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'PIX3L-IT', // Usually your GitHub org/user name.
-  projectName: 'Documentacion', // Usually your repo name.
+  projectName: 'Wiki', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
